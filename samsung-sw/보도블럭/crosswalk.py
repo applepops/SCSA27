d441832ea6660662b61f7c4bcc2b0ff70621b906
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-08-27 20:24:11 +0900

    [Passed] 보도블럭 (90ms, 18MB)

--- samsung-sw/보도블럭/crosswalk.py
+++ samsung-sw/보도블럭/crosswalk.py
@@ -1,10 +1,26 @@
 #'지나갈 수 있는 행과 열'의 개수 구하기
 #경사로를 두지 않은 경우에도 지나갈 수 있을 수 있음.
 
-#순회를 싹하면서 값 차이가 0.5 초과하면 False 아니면 True 반환
-def check_reachable(lst):
-    for i in range (len(lst)-1):
-        if abs(lst[i] - lst[i+1]) > 0.5:
+def check_way (n_arr):
+    for i in range (1, len(n_arr)):
+        d = abs(n_arr[i-1] - n_arr[i])
+        #두 값이 같으면 지나간다.
+        if d == 0:
+            continue
+        #1 차이가 나면 작은 애의 stairs를 확인한다.
+        elif d == 1:
+            if n_arr[i-1] < n_arr[i]:
+                if stairs[i-1] == 1:
+                    continue
+                else:
+                    return False
+            else:
+                if stairs[i] == 1:
+                    continue
+                else:
+                    return False
+        #두 값 차이가 2 이상이면 걍 안되는 놈이다.
+        else:
             return False
     else:
         return True
@@ -25,8 +41,9 @@ ans = 0
 
 for mini_new_arr in new_arr:
 
-    if check_reachable(mini_new_arr):
-        # print(mini_new_arr)
+    stairs = [0] * N
+
+    if check_way(mini_new_arr):
         ans += 1
         continue
 
@@ -42,8 +59,10 @@ for mini_new_arr in new_arr:
                 continue
         #다 같은 애들임이 판별났다.
         else:
+
             put_stairs_front = False
             put_stairs_back = False
+
             if 0 <= i-1 < N:
                 if mini_new_arr[i-1] == tmp[0]+1:
                     put_stairs_front = True
@@ -52,19 +71,25 @@ for mini_new_arr in new_arr:
                 if mini_new_arr[i+L] == tmp[0]+1:
                     put_stairs_back = True
 
+            #앞에도 뒤에도 다 큰 놈이 있어서 계단을 넣을 수 없다
             if put_stairs_front and put_stairs_back:
                 continue
+            #앞에도 뒤에도 다 큰 놈이 없다
             elif not put_stairs_front and not put_stairs_back:
                 continue
             else:
                 for plus in range (i, i+L):
-                    mini_new_arr[plus] += 0.5
+                    #계단이 이미 있다 미친놈아 나가라
+                    if stairs[plus] == 1:
+                        break
+                    else:
+                        stairs[plus] = 1
 
-                #값을 더해주고 나서 지나갈 수 있는 길이 되었는지 확인하자..
-                if check_reachable(mini_new_arr):
-                    # print(mini_new_arr)
-                    ans += 1
-                    break
+    if check_way(mini_new_arr):
+        # print("나는 체크되었다.")
+        # print(stairs)
+        # print(mini_new_arr)
+        ans += 1
 
 
 print(ans)
