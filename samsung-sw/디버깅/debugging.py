02f5d3d3fb43cc9f438ce6b9c5560dcacd7bd69b
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-08-27 17:43:31 +0900

    [Passed] 디버깅 (74ms, 17MB)

--- samsung-sw/디버깅/debugging.py
+++ samsung-sw/디버깅/debugging.py
@@ -60,6 +60,21 @@ def backtracking2(n, start_idx):
         backtracking2(n+1, i+1)
         tmp_lst.pop()
 
+def backtracking3(n, start_idx):
+    if n == 3:
+        tmp_tmp_lst = [row[:] for row in tmp_lst[:]]
+        tmp_tmp_lst = sorted(tmp_tmp_lst, key=lambda x: x[0])
+        if check_no_bug(tmp_tmp_lst):
+            print(3)
+            exit()
+        else:
+            return
+
+    for i in range (start_idx, len(fix_bug_lines_hubo)):
+        tmp_lst.append(fix_bug_lines_hubo[i])
+        backtracking3(n+1, i+1)
+        tmp_lst.pop()
+
 
 #고객의 수(세로선 개수), 메모리 유실 선의 개수(버그 개수), 취약 지점의 개수(가로선 개수)
 N, M, H = map(int, input().split())
@@ -99,6 +114,7 @@ tmp_lst = [row[:] for row in bug_lines[:]]
 
 backtracking1(0)
 backtracking2(0, 0)
+backtracking3(0, 0)
 
 print(-1)
 
