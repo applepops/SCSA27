7ef2f2fa4f3781a20ca890ed1eeccb28af0b4a8a
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-08-28 10:20:36 +0900

    [Time Limit Exceed] 바이러스 실험 (2072ms, 32MB)

--- samsung-sw/바이러스 실험/virus-experiment.py
+++ samsung-sw/바이러스 실험/virus-experiment.py
@@ -1,5 +1,5 @@
 from collections import deque
-from heapq import heappop, heappush
+#from heapq import heappop, heappush
 
 #배지의 크기 N*N, 바이러스 개수 m, 사이클 개수 k
 N, M, K = map(int, input().split())
@@ -15,36 +15,33 @@ yangboon_by_cycle = [list(map(int, input().split())) for _ in range (N)]
 #초기 양분 상황
 current_yangboon = [[5] * N for _ in range (N)]
 
-hq = []
+hq = deque()
 for _ in range (M):
     vr, vc, age = map(int, input().split())
     #0-based로 만들어주기
     vr -= 1
     vc -= 1
     #age를 먼저 넣어준다! 어린 놈부터 꺼내서 양분 먹어야 해!
-    heappush(hq, [age, vr, vc])
+    hq.append([age, vr, vc])
 
 #K번 동안 반복할 거임
 for _ in range (K):
 
     dead_virus_yangboon = [[0] * N for _ in range (N)]
-    new_hq = []
-    q = deque()
+    new_hq = deque()
+
+    hq = sorted(hq, key=lambda x: x[0])
 
     #1) hq에 있는 애들 다 꺼내서 양분 섭취시키거나 죽인다
     for _ in range (len(hq)):
         #바이러스 한 놈 꺼낸다.
-        n_age, nr, nc = heappop(hq)
+        n_age, nr, nc = hq.pop(0)
         #바이러스의 나이만큼 이곳에 양분이 충분히 있다면.
         if current_yangboon[nr][nc] >= n_age:
             current_yangboon[nr][nc] -= n_age
             n_age += 1
             #살아남은 놈들은 새로운 hq에 저장해준다.
-            heappush(new_hq, [n_age, nr, nc])
-
-            #나이가 5의 배수가 된 놈들은 따로 큐에 추가로 저장해준다.
-            if n_age % 5 == 0:
-                q.append([n_age, nr, nc])
+            new_hq.append([n_age, nr, nc])
 
         #바이러스가 죽는 경우
         else:
@@ -55,25 +52,23 @@ for _ in range (K):
         for j in range (N):
             current_yangboon[i][j] += dead_virus_yangboon[i][j]
 
-
     #3) 나이가 5의 배수인 놈들이 번식한다.
-    while q:
-        c_age, cr, cc = q.pop()
+    for v in range (len(new_hq)):
+        if new_hq[v][0] % 5 == 0:
+            for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)):
+                ni = new_hq[v][1] + di
+                nj = new_hq[v][2] + dj
 
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)):
-            ni = cr + di
-            nj = cc + dj
-
-            #범위 내인지 확인
-            if 0 <= ni < N and 0 <= nj < N:
-                heappush(new_hq, [1, ni, nj])
+                #범위 내인지 확인
+                if 0 <= ni < N and 0 <= nj < N:
+                    new_hq.append([1, ni, nj])
 
     #4) 주어진 양분의 양에 따라 칸에 양분 추가
     for i in range (N):
         for j in range (N):
             current_yangboon[i][j] += yangboon_by_cycle[i][j]
 
-    #힙큐를 스왑해준다.
+    #큐를 스왑해준다.
     hq = new_hq
 
 #살아있는 바이러스의 양..
