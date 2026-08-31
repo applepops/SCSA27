3448a969a8ec5700510e9382faf6737c5e8b2744
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-08-31 15:16:51 +0900

    [Passed] 이상한 다트 게임 (133ms, 20MB)

--- samsung-sw/이상한 다트 게임/odd-dart-game.py
+++ samsung-sw/이상한 다트 게임/odd-dart-game.py
@@ -1,4 +1,6 @@
 #회전을 다 돌고 나서 지우는 건 한 번에 한다는 걸까? -> 예시 보니까 그런듯.
+#아니 근데 그러면 왜 '원판에 남은 수가 없을 경우에는' 정규화를 진행하지 않는다고 하지?
+#n하고 m은 최소 2씩이라 원판에 남은 수가 없을 수가 있나?
 #원판의 number는 1-based 주의
 
 def rotate_wonpans (xx, dd, kk):
@@ -20,8 +22,15 @@ def rotate_wonpans (xx, dd, kk):
 
 
 def delete_numbers ():
+    need_to_delete = [[0] * M for _ in range(N + 1)]
+
     for i in range (1, N+1):
         for j in range (M):
+
+            #이미 지워진 애면.. 넘어가..
+            if arr[i][j] == 0:
+                continue
+
             #한 원판 안에서의 양쪽 옆 비교
             if arr[i][j] == arr[i][(j + 1) % M]:
                 need_to_delete[i][j] = 1
@@ -48,6 +57,14 @@ def delete_numbers ():
                     need_to_delete[i][j] = 1
                     need_to_delete[i + 1][j] = 1
 
+    #만약에 지워져야 하는 애면 0으로..
+    for i in range (1, N+1):
+        for j in range (M):
+            if need_to_delete[i][j] == 1:
+                arr[i][j] = 0
+
+    return sum(map(sum, need_to_delete))
+
 def print_arr ():
     for row in arr:
         print(*row)
@@ -58,36 +75,38 @@ N, M, Q = map(int, input().split())
 
 #원판 number를 1-based로 만들어주기 위해서 앞에다가 더미 붙였음.
 arr = [[0] * M] + [list(map(int, input().split())) for _ in range (N)]
+need_to_delete = [[0] * M for _ in range(N + 1)]
 
-need_to_delete = [[0] * M for _ in range (N+1)]
+total_num_cnt = N * M
 
 for _ in range (Q):
     x, d, k = map(int, input().split())
     rotate_wonpans(x, d, k)
+    deleted_num_cnt = delete_numbers()
 
-delete_numbers()
+    #지워진 숫자가 없는 경우
+    if deleted_num_cnt == 0 and total_num_cnt > 0:
+        total_mean = sum(map(sum, arr)) // total_num_cnt
+        for i in range (1, N+1):
+            for j in range (M):
+                if arr[i][j] == 0:
+                    continue
+                if arr[i][j] > total_mean:
+                    arr[i][j] -= 1
+                elif arr[i][j] < total_mean:
+                    arr[i][j] += 1
+                else:
+                    continue
+    else:
+        total_num_cnt -= deleted_num_cnt
 
-#지워진 숫자가 없는 경우
-if sum(map(sum, need_to_delete)) == 0:
-    total_mean = sum(map(sum, arr)) // (N * M)
-    for i in range (1, N+1):
-        for j in range (M):
-            if arr[i][j] > total_mean:
-                arr[i][j] -= 1
-            elif arr[i][j] < total_mean:
-                arr[i][j] += 1
-            else:
-                continue
 
-    print(sum(map(sum, arr)))
-#지워진 숫자가 있는 경우
-else:
-    res = 0
-    for i in range (1, N+1):
-        for j in range (M):
-            if need_to_delete[i][j] == 0:
-                res += arr[i][j]
+res = 0
+for i in range (1, N+1):
+    for j in range (M):
+        if arr[i][j] != 0:
+            res += arr[i][j]
 
-    print(res)
+print(res)
 
 
