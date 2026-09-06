6ff761fa12a5f146cb521f282dcd7bbd691b5502
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-09-06 19:50:57 +0900

    [Passed] 외주 수익 최대화하기 (81ms, 19MB)

--- samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,43 +1,27 @@
-def backtracking(n, profit_sum):
+def backtracking (n, cur_profit):
+    global max_profit
 
-    global max_profit_sum
-
-    #N일까지는 처리해야 하니까 종료조건은 N+1
-    if n == N+1:
-        for i in range (1, N+1):
-            if visited[i] >= 2:
-                break
-        else:
-            max_profit_sum = max(max_profit_sum, profit_sum)
+    if n > N+1:
         return
 
-    #n 날짜에 외주 작업 수행함
-    if (n+works[n][0]) <= N+1:
-        for i in range (n, n+works[n][0]):
-            visited[i] += 1
-        backtracking(n+1, profit_sum + works[n][1])
-        #원상복구
-        for i in range (n, n+works[n][0]):
-            visited[i] -= 1
-    #n 날짜에 외주 작업 수행 안함
-    backtracking(n+1, profit_sum)
+    if n == N+1:
+        max_profit = max(cur_profit, max_profit)
+        return
 
-#부분집합?
-#전체 날짜
+    t, p = works[n]
+    backtracking(n+t, cur_profit+p) #n일째의 일을 넣는다
+    backtracking(n+1, cur_profit) #n일째의 일을 안 넣는다.
+
+#입력받기
+#N일
 N = int(input())
+works = [[] for _ in range (N+1)] #1-based를 위해 dummy
+for n in range (1, N+1):
+    t, p = map(int, input().split())
+    works[n] = [t, p]
 
-#각 일자에 수행하는 외주 작업의 기한과 수익 입력 받기
-#각 일자는 1-based. 맨앞은 더미
-works = [[] for _ in range (N+1)]
-
-for i in range (1, N+1):
-    days, profit = map(int, input().split())
-    works[i] = [days, profit]
-
-visited = [0] * (N+1)
-max_profit_sum = -float("INF")
-#날짜, 수익 합계
+max_profit = 0
 backtracking(1, 0)
 
-print(max_profit_sum)
+print(max_profit)
 
