03e8203dd3c2539166ef9a01aaf94d4e782e53e8
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-09-08 16:33:28 +0900

    [Wrong Answer] 팩맨 (51ms, 16MB)

--- samsung-sw/팩맨/pacman.py
+++ samsung-sw/팩맨/pacman.py
@@ -4,27 +4,32 @@
 #기억 못할 것은 적어둔다.
 
 def backtracking(n, cur_cnt, pi, pj, way_lst):
-
     global max_monster_cnt
     global packman_next_way
 
-    #미친. 상좌하우 우선순위가 안 지켜져.
     if n == 3:
         if max_monster_cnt < cur_cnt:
             max_monster_cnt = cur_cnt
             packman_next_way = way_lst[:]
         return
 
-    #출발지 몬스터는 안 잡아 먹음.
-    for i in [1, 3, 5, 7]:
-        ni = pi + didj.get(i, 0)[0]
-        nj = pj + didj.get(i, 0)[1]
-        if 0 <= ni < 4 and 0 <= nj < 4 and visited[ni][nj] == 0: #범위 확인
+    for i in [1, 3, 5, 7]:  # 상 좌 하 우
+        ni = pi + didj[i][0]
+        nj = pj + didj[i][1]
+
+        if 0 <= ni < 4 and 0 <= nj < 4:
+
             way_lst.append(i)
-            visited[ni][nj] = 1
-            backtracking(n+1, cur_cnt+tmp_alive_monsters_arr[ni][nj], ni, nj, way_lst[:])
+
+            if visited[ni][nj] == 0:
+                visited[ni][nj] = 1
+                backtracking(n + 1, cur_cnt + tmp_alive_monsters_arr[ni][nj], ni, nj, way_lst)
+                visited[ni][nj] = 0
+            else: #이미 방문한 적 있던 곳이면 몬스터 새로 안 잡아먹음.
+                backtracking(n + 1, cur_cnt, ni, nj, way_lst)
+
             way_lst.pop()
-            visited[ni][nj] = 0
+
 
 
 didj = {1: (-1, 0), 2: (-1, -1), 3: (0, -1), 4: (1, -1), 5: (1, 0), 6: (1, 1), 7: (0, 1), 8: (-1, 1)}
@@ -46,13 +51,11 @@ for _ in range (M):
 #4*4 배열
 #t턴 동안 반복한다.
 for t in range (T):
-
-
-    #0. 몬스터 시체 한 턴 빼주기? 아마? 아닌가?
-    for i in range (4):
-        for j in range (4):
-            if dead_monsters[i][j] > 0:
-                dead_monsters[i][j] -= 1
+    # print("======================")
+    # print(f"{t+1}턴입니다.")
+    # print(f"팩맨은 현재: {packman_i} {packman_j}")
+    #
+    # print(alive_monsters)
 
     #1. 몬스터 복제 시도
     #현재 위치에서 자신과 같은 방향을 가진 몬스터를 복제.
@@ -62,7 +65,7 @@ for t in range (T):
 
     #2. 몬스터 이동
     #한 칸 이동하되, 몬스터 시체가 없고 팩맨이 없고 격자를 벗어나면 안됨.
-    #안되는 경우 (d + 1) %8 하고 그걸 8번해도 안되면 움직이지 않고 유지
+    #안되는 경우 방향 바꾸고 그걸 8번해도 안되면 움직이지 않고 유지
     for i in range (len(alive_monsters)):
         ci, cj, cd = alive_monsters[i][0], alive_monsters[i][1], alive_monsters[i][2]
 
@@ -93,6 +96,9 @@ for t in range (T):
     for r, c, d in alive_monsters:
         tmp_alive_monsters_arr[r][c] += 1
 
+    # for row in tmp_alive_monsters_arr:
+    #     print(*row)
+
     #3. 팩맨 이동
     #백트래킹. 상, 좌, 하, 우 순으로 순열로 3개 뽑기. -> 가장 많은 방향 3개 lst를 반환.
     max_monster_cnt = 0
@@ -102,9 +108,9 @@ for t in range (T):
 
     visited = [[0] * 4 for _ in range (4)]
     backtracking(0, 0, packman_i, packman_j, tmp_lst)
+    # print("팩맨 이동경로")
+    # print(packman_next_way)
 
-
-    #4. 몬스터 시체 소멸
     # 그 lst에 따라서 팩맨이 움직이면서 애들 죽인다.
     # 몬스터의 시체를 남긴다. dead_monsters 배열에 2를 추가.
     new_alive_monsters = []
@@ -131,9 +137,26 @@ for t in range (T):
 
     alive_monsters = new_alive_monsters
 
+    # print("팩맨이 지나가고.. 살아남은 애들")
+    # print(alive_monsters)
+
+    #4. 몬스터 시체 소멸
+    for i in range (4):
+        for j in range (4):
+            if dead_monsters[i][j] == 2:
+                continue
+            elif dead_monsters[i][j] > 0:
+                dead_monsters[i][j] -= 1
+
+    # print("시체들.")
+    # for row in dead_monsters:
+    #     print(*row)
+
     #5. 몬스터 복제 완성
     #알들이 깨어난다. alive_monsters에 넣어주자.
     alive_monsters = alive_monsters + eggs
+    # print("알들이 깨어났다.")
+    # print(alive_monsters)
 
 
 #다 끝나고 나면..
