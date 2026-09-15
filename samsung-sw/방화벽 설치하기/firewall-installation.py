9679ebe656ae50c47bb3abf06481446a2b6710f3
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-09-15 20:17:08 +0900

    [Passed] 방화벽 설치하기 (489ms, 23MB)

--- samsung-sw/방화벽 설치하기/firewall-installation.py
+++ samsung-sw/방화벽 설치하기/firewall-installation.py
@@ -1,73 +1,80 @@
-#방화벽을 제외하고 불이 퍼지지 않는 영역 크기의 최댓값
-#전체 크기 - 방화벽 총 개수 - 불 퍼진 영역 개수가 가장 큰 것을 출력하자.
 from collections import deque
 
-def bfs ():
-    q = deque()
-    visited = [[0] * M for _ in range (N)]
+def bfs(wall_lst):
 
     fire_cnt = 0
+    tmp_arr = [row[:] for row in arr]
+    for i, j in wall_lst:
+        tmp_arr[i][j] = 1
 
-    for fire_i, fire_j in fires:
-        q.append((fire_i, fire_j))
-        visited[fire_i][fire_j] = 1
+    # print()
+    # for row in tmp_arr:
+    #     print(*row)
 
-    while q:
 
-        cur_i, cur_j = q.pop()
-        fire_cnt += 1
+    visited = [[0] * M for _ in range(N)]
 
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            ni = cur_i + di
-            nj = cur_j + dj
+    for i in range (N):
+        for j in range (M):
+            if tmp_arr[i][j] == 2 and visited[i][j] == 0:
 
-            #이동 가능한 좌표인가
-            if 0 <= ni < N and 0 <= nj < M:
-                #빈칸이면, 그리고 아직 불이 닿은 적 없으면.
-                if arr[ni][nj] == 0 and visited[ni][nj] == 0:
-                    visited[ni][nj] = 1
-                    q.append((ni, nj))
+                q = deque()
+                q.append([i, j])
+                visited[i][j] = 1
+
+                while q:
+
+                    ci, cj = q.popleft()
+                    fire_cnt += 1
+
+                    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                        ni = di + ci
+                        nj = dj + cj
+
+                        if 0 <= ni < N and 0 <= nj <  M and visited[ni][nj] == 0 and tmp_arr[ni][nj] == 0:
+                            q.append([ni, nj])
+                            visited[ni][nj] = 1
+
+    # print(f"불 개수: {fire_cnt}")
 
     return fire_cnt
 
 
+def backtracking(n, idx):
+    global min_fire_cnt
+    global wall_cnt
+
+    if n == 3:
+        res = bfs(lst[:])
+        if min_fire_cnt > res:
+            min_fire_cnt = res
+
+        return
+
+    for h in range (idx, len(hubo)):
+        lst.append(hubo[h])
+        backtracking(n+1, h+1)
+        lst.pop()
+
+
+
 N, M = map(int, input().split())
-
-#영역 입력받기
 arr = [list(map(int, input().split())) for _ in range (N)]
+min_fire_cnt = float("inf")
+wall_cnt = 0
 
-fires = [] #불들의 위치 기억하려고
-walls = [] #기존 방화벽들의 위치 기억하려고
-blanks = [] #얘가 방화벽이 될 후보지들이 된다.
-
-max_cnt = 0
-
+hubo = []
 for i in range (N):
     for j in range (M):
         if arr[i][j] == 0:
-            blanks.append((i, j))
-        elif arr[i][j] == 1:
-            walls.append((i, j))
-        elif arr[i][j] == 2:
-            fires.append((i, j))
+            hubo.append([i, j])
+        if arr[i][j] == 1:
+            wall_cnt += 1
 
 
-for i in range (len(blanks)-2):
-    for j in range (i+1, len(blanks)-1):
-        for k in range (j+1, len(blanks)):
-            #방화벽 추가하기...
-            arr[blanks[i][0]][blanks[i][1]] = 1
-            arr[blanks[j][0]][blanks[j][1]] = 1
-            arr[blanks[k][0]][blanks[k][1]] = 1
+lst = []
+backtracking(0, 0)
 
-            res = bfs()
-            #전체 영역 크기 - 불 붙은 영역 - 방화벽이 있는 영역 = 불 안 퍼진 영역
-            temp = N*M - res - (len(walls) + 3)
-            max_cnt = max(temp, max_cnt)
+# print(min_fire_cnt)
 
-            #다시 빈칸으로 되돌리기...
-            arr[blanks[i][0]][blanks[i][1]] = 0
-            arr[blanks[j][0]][blanks[j][1]] = 0
-            arr[blanks[k][0]][blanks[k][1]] = 0
-
-print(max_cnt)
+print(N*M - min_fire_cnt - wall_cnt - 3)
