91ad4d117e1bd1a3b11ac8f2b4607fbc33a8d79c
Jihee Hwang <101245685+applepops@users.noreply.github.com>
2026-09-21 15:54:22 +0900

    [Passed] 미지의 공간 탈출 (57ms, 17MB)

--- samsung-sw/미지의 공간 탈출/escape-unknown-space.py
+++ samsung-sw/미지의 공간 탈출/escape-unknown-space.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+#from pprint import pprint
 didj = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def return_arr(num):
@@ -46,106 +46,91 @@ def move(ci, cj, now_arr, now_arr_num, now_d):
                     now_arr_num = 12
                     ni = 0
                     nj = cj
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif ni == -1:
                     now_arr_num = 13
                     ni = 0
                     nj = M-cj-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == -1:
                     now_arr_num = 14
                     ni = 0
                     nj = ci
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == M:
                     now_arr_num = 15
                     ni = 0
                     nj = M-ci-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
             elif now_arr_num == 12:
                 if ni == -1:
                     now_arr_num = 11
                     ni = M-1
                     nj = cj
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == -1:
                     now_arr_num = 14
                     ni = ci
                     nj = M-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == M:
                     now_arr_num = 15
                     ni = ci
                     nj = 0
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif ni == M:
                     now_arr_num = 16
                     ni = ei
                     nj = cj + sj
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
             elif now_arr_num == 13:
                 if ni == -1:
                     now_arr_num = 11
                     ni = 0
                     nj = M-cj-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == -1:
                     now_arr_num = 15
                     ni = ci
                     nj = M-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == M:
                     now_arr_num = 14
                     ni = ci
                     nj = 0
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif ni == M:
                     now_arr_num = 16
                     ni = si -1
                     nj = sj + M -cj -1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
             elif now_arr_num == 14:
                 if ni == -1:
                     now_arr_num = 11
                     ni = cj
                     nj = 0
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == -1:
                     now_arr_num = 13
                     ni = ci
                     nj = M-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == M:
                     now_arr_num = 12
                     ni = ci
                     nj = 0
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif ni == M:
                     now_arr_num = 16
                     ni = si + cj
                     nj = sj-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
             elif now_arr_num == 15:
                 if ni == -1:
                     now_arr_num = 11
                     ni = cj
                     nj = M-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == M:
                     now_arr_num = 13
                     ni = ci
                     nj = 0
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif nj == -1:
                     now_arr_num = 12
                     ni = ci
                     nj = M-1
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif ni == M:
                     now_arr_num = 16
                     ni = ei - 1 - cj
                     nj = ej
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
 
 
     else: #평면
@@ -157,28 +142,26 @@ def move(ci, cj, now_arr, now_arr_num, now_d):
                     now_arr_num = 12
                     ni = M-1
                     nj = cj - sj
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif now_d == 2 and ni == si:
                     now_arr_num = 13
                     ni = M-1
                     nj = M - 1 - (cj - sj)
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif now_d == 0 and nj == sj:
                     now_arr_num = 14
                     ni = M-1
                     nj = ci - si
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
                 elif now_d == 1 and nj == ej -1:
                     now_arr_num = 15
                     ni = M-1
                     nj = ei - 1 - ci
-                    return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
+                return ni, nj, return_arr(now_arr_num), now_arr_num, now_d
 
         else: #아예 격자를 벗어남. 못 간다.
             return -1, -1, now_arr, now_arr_num, now_d
 
     pass
 
+#시간의 벽 시작점과 끝점 찾기 -> 범위 알려고
 def find_se():
     for i in range(N):
         for j in range(N):
@@ -215,6 +198,7 @@ time_machine_ijs = []
 time_machine_ijs.append([tm_i, tm_j, 11])
 visited11[tm_i][tm_j] = 1
 
+#도착해야 하는 지점 찾기
 goal_i, goal_j = -1, -1
 for i in range (N):
     for j in range (N):
@@ -228,18 +212,11 @@ for _ in range (F):
     arr16[r][c] = 9
 
 si, sj, ei, ej = find_se()
-# print(si, sj, ei, ej)
-#단위테스팅 드가자...
-# print(move(4, 1, arr16, 16, 0))
-
-#지금 코드는 다른 arr로 이동할 때 그곳에서의 장애물 / 시간 이상 현상을 피하지는 않고 있음
-#나중에 수정 필요함.
 
 ans = 0
 is_found = False
 while True:
     ans += 1
-    # print(f"{ans}번째")
 
     #시간 이상 현상이 움직인다.
     new_time_wrong_points = []
@@ -249,7 +226,7 @@ while True:
             if now_arr[nr][nc] == 0: #이제야 장애물 확인함.
                 now_arr[nr][nc] = 9
                 new_time_wrong_points.append([nr, nc, n_d, v, n_arr_n])
-        else: #차례가 아니면 기다리는 거고..
+        else: #차례가 아니면 다시 넣어서 기다리는 거고..
             new_time_wrong_points.append([r, c, d, v, arr_n])
 
     time_wrong_points = new_time_wrong_points
@@ -261,24 +238,22 @@ while True:
         for d in [0, 1, 2, 3]:
             nr, nc, now_arr, n_arr_n, n_d = move(time_machine_ijs[i][0], time_machine_ijs[i][1], return_arr(time_machine_ijs[i][2]), time_machine_ijs[i][2], d)
 
-            # print(f"{time_machine_ijs[i][0], time_machine_ijs[i][1], time_machine_ijs[i][2]}에서 넘어감.")
-            # print(nr, nc, n_arr_n)
-
+            #도착지점에 오면 True -> 끝내기
             if (nr, nc, n_arr_n) == (goal_i, goal_j, 16):
                 is_found = True
 
+            #다음 좌표가 격자외면 continue
             if (nr, nc) == (-1, -1):
                 continue
 
             if 0 <= nr < len(now_arr) and 0 <= nc < len(now_arr) and now_arr[nr][nc] == 0 and return_visited(n_arr_n)[nr][nc] == 0: #장애물 확인 및 안 가본 곳인지 확인.
                 return_visited(n_arr_n)[nr][nc] = 1
                 new_time_machine_ijs.append([nr, nc, n_arr_n])
+
     time_machine_ijs = new_time_machine_ijs
 
     if not time_machine_ijs:
         break
-
-    # pprint(visited16)
 
     if is_found:
         break
